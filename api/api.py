17fcdfadeb5101003533a03d22db8c7d8b67a2a9
mattrossman <mrossman@umass.edu>
2019-11-19 14:16:31 -0500

    Add skeleton for articles endpoint

--- api/api.py
+++ api/api.py
@@ -4,3 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/articles', methods=['POST'])
+def articles():
+    """ Get list of articles based on sentiment threshold, whitelist, and blacklist """
+    ...
