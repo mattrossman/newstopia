02845d0b4faf7f9bf5b8012b3e73c37fd830efa0
Ayush Khandelwal <ayushkhd@1x-nat-vl930-172-30-90-76.wireless.umass.edu>
2019-11-21 15:41:21 -0500

    Category list

--- News.py
+++ News.py
@@ -3,7 +3,28 @@ import json
 
 api = NewsApiClient(api_key='5abd84eed9594c6390e96ec552d38a37')
 
-topNews = api.get_top_headlines(sources='bbc-news')
-print(json.dumps(topNews, indent=4))
+# topNews = api.get_top_headlines(sources='bbc-news')
+# print(json.dumps(topNews, indent=4))
 
 # print(json.dumps(api.get_sources(),indent = 4))    
+
+sources = dict
+sources = (api.get_sources(language="en"))
+# print(json.dumps(sources, indent = 4))
+
+
+print(sources['sources'][0]['category'])
+
+a = 0
+catergoryCount = dict()
+categories = list()
+
+for x in sources['sources']:
+    categories.append(x['category'])
+    print (x['name'] + "     Category: " + x['category'])
+    if catergoryCount.__contains__(x['category']):
+        catergoryCount[x['category']] = catergoryCount.get(x['category'])+1
+    else:
+        catergoryCount[x['category']] = 1
+
+print(catergoryCount.keys())
