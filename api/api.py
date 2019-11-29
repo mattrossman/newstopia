177ebb796a6f3f12815046e02f01dc97a37bde16
mattrossman <mrossman@umass.edu>
2019-11-29 15:04:27 -0500

    Plug news filtering into flask

--- api/api.py
+++ api/api.py
@@ -1,16 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+from news import fetch_filtered_articles
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route('/articles', methods=['POST'])
 def articles():
-    """ Get list of articles based on sentiment threshold, whitelist, and blacklist """
-    dummy_articles = [
-        'https://www.cnn.com/2019/11/21/politics/gordon-sondland-testimony/index.html',
-        'https://www.nbcnews.com/politics/2020-election/top-2020-democrats-played-it-safe-atlanta-debate-n1088336',
-        'https://www.independent.co.uk/news/science/dark-matter-particle-hungary-atomki-nuclear-research-force-nature-a9210741.html'
-    ]
-    return {'articles': dummy_articles}
+    """Get list of articles based on sentiment threshold, whitelist, and blacklist
+
+    JSON Body
+    ---------
+    threshold : float
+    whitelist : List[str]
+    blacklist : List[str]
+
+    """
+    return {'articles': fetch_filtered_articles(**request.json)}
