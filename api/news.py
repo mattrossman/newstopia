b4456617ddc426e1a230255752a5f822a7d098b1
mattrossman <mrossman@umass.edu>
2019-11-29 12:24:31 -0500

    Move updated News.py content into news.py

--- api/news.py
+++ api/news.py
@@ -1,42 +1,35 @@
 from newsapi import NewsApiClient
-import json
 from typing import List, TypedDict
 
 api = NewsApiClient(api_key='5abd84eed9594c6390e96ec552d38a37')
 
 sources = api.get_sources(language="en")
 
-categoryDict = dict() #list of sources available by category, we could have a form of an updating list which shows which sources are shown after selecting a certain category
-listOfSources = list() #list of all news sources available to choose from
+categoryDict = dict()  # list of sources available by category, we could have a form of an updating list which shows which sources are shown after selecting a certain category
+listOfSources = list()  # list of all news sources available to choose from
 # print(sources['sources'][0]['category'])
 
 for i in sources['sources']:
-    s = str(i['name']) 
-    listOfSources.append(str(i['name']) )
+    s = str(i['name'])
+    listOfSources.append(str(i['name']))
     if not categoryDict.__contains__(str(i['category'])):
         categoryDict[str(i['category'])] = [s]
     else:
         categoryDict[str(i['category'])].append(s)
-        
 
-whiteListKeywords = [] #user entered keywords for whitelisting
-blackListKeywords = set() #user entered keywords for whitelisting
+whiteListKeywords = []  # user entered keywords for whitelisting
+blackListKeywords = set()  # user entered keywords for whitelisting
 
-top_headlines = api.get_top_headlines(country = 'us', category ='business') #pulls for categories selected only, will change
+top_headlines = api.get_top_headlines(country='us',
+                                      category='business')  # pulls for categories selected only, will change
 
-
+filteredArticles = dict()  # key - article id , value - [title, description, author, content, url, imageUrl]
 # breaking down into variables to make it easier to use
-for i in top_headlines['articles']:
-    title = i['title']
-    description = i['description']
-    url = i['url']
-    author = i['author']
-    content = i['content'] #only first 2 lines of article, can be used below description
-    blackListCheck(title)
-    blackListCheck(description)
+articleId = 0
 
-
+# Not using 'class' notation here due to reserved word 'id'
 SourceInfo = TypedDict('SourceInfo', {'id': str, 'name': str})
+
 
 class ArticleInfo(TypedDict):
     source: SourceInfo
@@ -47,6 +40,7 @@ class ArticleInfo(TypedDict):
     urlToImage: str
     publishedAt: str
     content: str
+
 
 class ResponseInfo(TypedDict):
     status: str
@@ -60,7 +54,7 @@ def fetch_top_articles() -> List[ArticleInfo]:
 
 def fetch_filtered_articles(sentiment: float, whitelist: List[str], blacklist: List[str]) -> List[ArticleInfo]:
     """Fetch the latest articles, filtered by sentiment and topics
-    
+
     Parameters
     ----------
     sentiment : float
@@ -76,20 +70,33 @@ def fetch_filtered_articles(sentiment: float, whitelist: List[str], blacklist: L
         Latest articles matching the filter settings
     """
     articles = fetch_top_articles()
-    ...
+    return ...
 
 
+def blackListCheck(s):
+    words = s.split(' ')
+    for i in words:
+        if blackListKeywords.__contains__(i):
+            return False
+
+    return True
 
 
+for i in top_headlines['articles']:
+    if i['title'] == None or i['description'] == None or i['url'] == None or i['urlToImage'] == None or i['author'] ==None or i['content'] == None:
+        continue
 
+    title = i['title'].encode('utf-8')
+    description = i['description'].encode('utf-8')
+    url = str(i['url'])
+    imageUrl = str(i['urlToImage'])
+    author = str(i['author'])
+    content = i['content'] #only first 2 lines of article, can be used below description
 
-
-
-
-
-
-
-
+    if not (blackListCheck(title) == True and blackListCheck(description) == True) :
+        continue
+    filteredArticles[articleId] = [title, description, author, content, url, imageUrl]
+    articleId += 1
 
 # topNews = api.get_top_headlines(sources='bbc-news')
 # print(json.dumps(topNews, indent=4))
